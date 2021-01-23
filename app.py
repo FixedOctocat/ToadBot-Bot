4f8e95861f811a1c8d2c739562bbe8e5e1dbe045
Fixed <egorfilatov2002@yandex.ru>
2021-01-23 03:35:58 +0300

    add lollipops and heals usage

--- app.py
+++ app.py
@@ -1,61 +1,31 @@
-# -*- coding: utf-8 -*-
-import json
-import vk_api
-from conf import access_token, chat_id
-from vk_api.utils import get_random_id
-from time import sleep
-
-commands = {
-	"perks": "Моя жаба",
-	"info": "Жаба инфо",
-	"feed": "Покормить жабу",
-	"mega_feed": "Откормить жабу",
-	"invent": "Мой инвентарь",
-	"heal": "Реанимировать жабу",
-	"work": "Отправить жабу на работу",
-	"end_work": "Завершить работу",
-}
-
-def login():
-	"""login via token"""
-	vk_session = vk_api.VkApi(token=access_token)
-	vk = vk_session.get_api()
-	return vk
-
-def get_info(vk):
-	"""send info command"""
-	vk.messages.send(chat_id=chat_id, message=commands["perks"], random_id=get_random_id())
-	info = vk.messages.getConversations(peer_id=2000000000+chat_id, count=1, random_id=get_random_id())["items"][0]["last_message"]["text"].split("\n")
-	vk.messages.markAsRead(peer_id=2000000000+chat_id, mark_conversation_as_read=1, random_id=get_random_id())
-
-	"""open and save data"""
-	with open('JabkaData.json') as f:
-		data = json.load(f)
-
-	data["xp"] = int(info[2][info[2].find(":")+2:info[2].find("/"):])
-	data["feel"] = info[7][info[7].find(":")+2::]
-	data["level"] = int(info[1][info[1].find(":")+2::])
-	data["money"] = int(info[5][info[5].find(":")+2::])
-	data["status"] = info[4][info[4].find(":")+4::]
-
-	vk.messages.send(chat_id=chat_id, message=commands["info"], random_id=get_random_id())
-	sleep(1)
-	info = vk.messages.getConversations(peer_id=2000000000+chat_id, count=1, random_id=get_random_id())["items"][0]["last_message"]["text"].split("\n")
-	vk.messages.markAsRead(peer_id=2000000000+chat_id, mark_conversation_as_read=1, random_id=get_random_id())
-
-	data["feed"] = info[0][info[0].find("з")+2::]
-	data["work"] = info[2][info[2].find("з")+2::]
-	if info[1].find("з") == -1:
-		data["mega_feed"] = 0
-	else:
-		data["mega_feed"] = info[1][info[1].find("з")+2::]
-	
-	with open('JabkaData.json', 'w', encoding='utf-8') as f:
-		json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
+from vk_connect import login, get_info, send_command
 
 def main():
 	vk = login()
-	get_info(vk)
+	data = get_info(vk)
+
+	if data["can_feed"] == 1:
+		send_command(vk, "feed")
+
+	if data["on_work"] == 0:
+		send_command(vk, "work")
+	elif data["on_work"] == 1:
+		send_command(vk, "end_work")
+
+	if data["status"] == "Нуждается в реанимации":
+		if data["heal"] > 0:
+			send_command(vk, "use_heal")
+		elif data["money"] >= 300:
+			send_command(vk, "buy_heal")
+			send_command(vk, "use_heal")
+
+	if data["feel"] == "Плохое":
+		if data["lollipop"] > 0:
+			send_command(vk, "use_lp")
+		elif data["money"] >= 300:
+			send_command(vk, "buy_lp")
+			send_command(vk, "use_lp")
+
 
 if __name__=='__main__':
 	main()
---
+++ vk_connect.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import json
+import vk_api
+from time import sleep
+from conf import access_token, chat_id
+from vk_api.utils import get_random_id
+
+commands = {
+	"perks": "Моя жаба",
+	"info": "Жаба инфо",
+	"feed": "Покормить жабу",
+	"mega_feed": "Откормить жабу",
+	"invent": "Мой инвентарь",
+	"heal": "Реанимировать жабу",
+	"work": "Отправить жабу на работу",
+	"end_work": "Завершить работу",
+	"buy_lp": "Приобрести леденцы 1",
+	"use_lp": "Использовать леденцы",
+	"buy_heal": "Приобрести аптечку",
+	"use_heal": "Реанимировать жабу",
+}
+
+def login():
+	"""login via token"""
+	vk_session = vk_api.VkApi(token=access_token)
+	vk = vk_session.get_api()
+	return vk
+
+def get_info(vk):
+	"""open and save data"""
+	with open('JabkaData.json') as f:
+		data = json.load(f)
+
+	"""send perks command"""
+	vk.messages.send(chat_id=chat_id, message=commands["perks"], random_id=get_random_id())
+	sleep(1)
+	info = vk.messages.getConversations(peer_id=2000000000+chat_id, count=1, random_id=get_random_id())["items"][0]["last_message"]["text"].split("\n")
+	vk.messages.markAsRead(peer_id=2000000000+chat_id, mark_conversation_as_read=1, random_id=get_random_id())
+
+	data["xp"] = int(info[2][info[2].find(":")+2:info[2].find("/"):])
+	data["feel"] = info[7][info[7].find(":")+2::]
+	data["level"] = int(info[1][info[1].find(":")+2::])
+	data["money"] = int(info[5][info[5].find(":")+2::])
+	data["status"] = info[4][info[4].find(":")+4::]
+
+	"""send info command"""
+	vk.messages.send(chat_id=chat_id, message=commands["info"], random_id=get_random_id())
+	sleep(1)
+	info = vk.messages.getConversations(peer_id=2000000000+chat_id, count=1, random_id=get_random_id())["items"][0]["last_message"]["text"].split("\n")
+	vk.messages.markAsRead(peer_id=2000000000+chat_id, mark_conversation_as_read=1, random_id=get_random_id())
+	
+	if "Можно покормить через" in info[0]:
+		data["can_feed"] = 0
+	elif "Жабу можно покормить" in info[0]:
+		data["can_feed"] = 1
+	
+	if "можно отправить на работу" in info[2]:
+		data["on_work"] = 0
+	elif "Можно забрать жабу с работы" in info[2]:
+		data["on_work"] = 1
+	elif "Забрать жабу можно через" in info[2]:
+		data["on_work"] = 2
+
+	if "Можно откормить" in info[1]:
+		data["mega_feed"] = 1
+	elif "Откормить через" in info[1]:
+		data["mega_feed"] = 0
+	
+	vk.messages.send(chat_id=chat_id, message=commands["invent"], random_id=get_random_id())
+	sleep(1)
+	info = vk.messages.getConversations(peer_id=2000000000+chat_id, count=1, random_id=get_random_id())["items"][0]["last_message"]["text"].split("\n")
+	vk.messages.markAsRead(peer_id=2000000000+chat_id, mark_conversation_as_read=1, random_id=get_random_id())
+	
+	data["heal"] = int(info[2][info[2].find(":")+2::])
+	data["lollipop"] = int(info[1][info[1].find(":")+2::])
+
+	with open('JabkaData.json', 'w', encoding='utf-8') as f:
+		json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
+
+	return data
+
+def send_command(vk, command):
+	vk.messages.send(chat_id=chat_id, message=commands[command], random_id=get_random_id())
+	sleep(1)
+	info = vk.messages.getConversations(peer_id=2000000000+chat_id, count=1, random_id=get_random_id())["items"][0]["last_message"]["text"].split("\n")
+	vk.messages.markAsRead(peer_id=2000000000+chat_id, mark_conversation_as_read=1, random_id=get_random_id())
+
+def main():
+	vk = login()
+	get_info(vk)
+
+if __name__=='__main__':
+	main()

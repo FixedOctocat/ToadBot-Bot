c2d55b845dafe61475631d9aa86e0176c2b1df5a
Fixed <egorfilatov2002@yandex.ru>
2021-02-01 11:09:07 +0300

    Add notifications and multiprocessing

--- WindowsApp.py
+++ WindowsApp.py
@@ -5,24 +5,60 @@ import PySimpleGUIQt as sg
 from tray_functions import start_JabkaBot, refresh_data
 from multiprocessing import Process, current_process
 
-menu_def = ['BLANK', ['&Start JabkaBot', '&Refresh data', '&Exit']]
+def main():
+	#system tray creation
+	menu_def = ['BLANK', ['&Start JabkaBot', '&Refresh data', '&Exit']]
+	tray = sg.SystemTray(menu=menu_def, filename='images/icon.png')
 
-tray = sg.SystemTray(menu=menu_def, filename='images/icon.png')
+	while True:
+		menu_item = tray.Read()
+		
+		if menu_item == 'Exit':
+			#end JabkaBot process
+			try:
+				JabkaBot.terminate()
+				JabkaBot.join()
+			except:
+				pass
 
-while True:
-	menu_item = tray.Read()
-	print(menu_item)
-	if menu_item == 'Exit':
-		break
-	elif menu_item == 'Start JabkaBot':
-		menu_def = ['BLANK', ['Stop JabkaBot', 'Refresh data', 'Exit']]
-		tray.Update(menu=menu_def, filename='images/icon.png')
-		
-		start_JabkaBot()
-	elif menu_item == 'Refresh data':
-		refresh_data()
-	elif menu_item == 'Stop JabkaBot':
-		menu_def = ['BLANK', ['Start JabkaBot', 'Refresh data', 'Exit']]
-		tray.Update(menu=menu_def, filename='images/icon.png')
-		
-		start_JabkaBot()
+			#end RefreshData process
+			try:
+				RefreshData.terminate()
+				RefreshData.join()
+			except:
+				pass
+
+			#quit
+			break
+		elif menu_item == 'Start JabkaBot':
+			#edit menu
+			menu_def = ['BLANK', ['Stop JabkaBot', 'Refresh data', 'Exit']]
+			tray.Update(menu=menu_def)
+			
+			#show notification
+			tray.ShowMessage("JabkaBot", "Жабкабот следит за Вашей жабкой", time=10)
+			
+			#start process
+			JabkaBot = Process(target=start_JabkaBot)
+			JabkaBot.start()
+		elif menu_item == 'Refresh data':
+			#show notification
+			tray.ShowMessage("JabkaBot", "Обновляем данные...", time=10)
+			
+			#start process
+			RefreshData = Process(target=refresh_data())
+			RefreshData.start()
+		elif menu_item == 'Stop JabkaBot':
+			#edit menu
+			menu_def = ['BLANK', ['Start JabkaBot', 'Refresh data', 'Exit']]
+			tray.Update(menu=menu_def)
+			
+			#show notification
+			tray.ShowMessage("JabkaBot", "Жабкабот больше не следит за жабкой", time=10)
+
+			#end process
+			JabkaBot.terminate()
+			JabkaBot.join()
+
+if __name__ == '__main__':
+	main()
